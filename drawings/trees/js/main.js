191f1c99c4b0ef35c484afcf25bb2b91ddbbb449
vstrimaitis <vstrimaitis@gmail.com>
2016-12-19 17:30:57 +0200

    Tree size now depends on the Y value of the mouse click

--- drawings/trees/js/main.js
+++ drawings/trees/js/main.js
@@ -14,8 +14,28 @@ function inRange(a, b){
     return Math.random() * (b-a) + a;
 }
 
+function getShrinkFactor(y){
+    var B = 15/7;
+    var A = 1209*window.innerHeight/49;
+    var d = -28*A/39;
+    return A/(y+d)+B;
+    //return (35/3 - 1/(4*y/window.innerHeight - 3)) / 16;
+}
+
+function getTrunkLength(y){
+    return 50*(3/(y/window.innerHeight-1.5)+7);
+}
+
 function onLeftClick(e){
-    var tree = new Tree(e.pageX, e.pageY, inRange(35, 60), inRange(2/3, 3/4), inRange(50, 250), inRange(10, 50), inRange(3, 10), inRange(0, 5), getRand(trunkColors), getRand(leafColors));
+    //var tree = new Tree(e.pageX, e.pageY, inRange(35, 60), inRange(2/3, 3/4), inRange(50, 250), inRange(10, 50), inRange(3, 10), inRange(0, 5), getRand(trunkColors), getRand(leafColors));
+    var params = {
+        base_x: e.pageX,
+        base_y: e.pageY,
+        shrink_factor: getShrinkFactor(e.pageY), // s(y) = A/(y+d1) + B
+        trunk_length: getTrunkLength(e.pageY)                                                     // l(y) = C/(y+d2) + D
+    };
+    //alert(e.pageY+": "+getShrinkFactor(e.pageY)+", "+getTrunkLength(e.pageY));
+    var tree = new Tree(params);
     console.log(JSON.stringify(tree));
     tree.build();
     tree.draw(ctx);
--- drawings/trees/js/tree.js
+++ drawings/trees/js/tree.js
@@ -1,14 +1,14 @@
-Tree = function(base_x, base_y, branching_angle, shrink_factor, trunk_length, trunk_width, min_branch_length, leaf_radius, wood_color, leaf_color){
-    this.base_x = base_x;
-    this.base_y = base_y;
-    this.branching_angle = branching_angle;
-    this.shrink_factor = shrink_factor;
-    this.trunk_length = trunk_length;
-    this.trunk_width = trunk_width;
-    this.min_branch_length = min_branch_length;
-    this.leaf_radius = leaf_radius;
-    this.wood_color = wood_color;
-    this.leaf_color = leaf_color;
+Tree = function(params/*base_x, base_y, branching_angle, shrink_factor, trunk_length, trunk_width, min_branch_length, leaf_radius, wood_color, leaf_color*/){
+    this.base_x = params.base_x || window.innerWidth/2;
+    this.base_y = params.base_y || window.innerHeight/2;
+    this.branching_angle = params.branching_angle || inRange(35, 60);
+    this.shrink_factor = params.shrink_factor || inRange(2/3, 3/4);
+    this.trunk_length = params.trunk_length || inRange(50, 250);
+    this.trunk_width = params.trunk_width || inRange(10, 50);
+    this.min_branch_length = params.min_branch_length || inRange(3, 10);
+    this.leaf_radius = params.leaf_radius || inRange(0, 5);
+    this.wood_color = params.wood_color || getRand(trunkColors);
+    this.leaf_color = params.leaf_color || getRand(leafColors);
 }
 
 Tree.prototype.root = null;
