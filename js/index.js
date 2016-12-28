43b7ecb18ebd3049414ac6cf777d2e9472c44470
vstrimaitis <vstrimaitis@gmail.com>
2016-12-28 16:10:29 +0200

    Redesigned front page

---
+++ js/index.js
@@ -0,0 +1,64 @@
+var Link = function(name, description, date, path, isDirect){
+    this.name = name;
+    this.description = description;
+    this.path = path;
+    this.isDirect = isDirect;
+    this.date = date;
+}
+
+var links = [
+    new Link("Analog clock", "An implementation of an animated analog clock.", "2016/12", "analogClock"),
+    new Link("Maze generator", "A demonstration of a maze generation algorithm.", "2016/12", "mazeGenerator"),
+    new Link("Dragon curve", "A visualization of the dragon curve.", "2016/12", "dragonCurve"),
+    new Link("Synced drawing", "An attempt of synchronizing drawings with Node.js.", "2016/12", "syncDraw"),
+    new Link("Solver", "A solver for word-search puzzles.", "2016/12", "wordSearchSolver"),
+    new Link("Idea bank", "A page for saving my ideas for future projects.", "2016/12", "ideas"),
+    new Link("Fractal trees", "A fractal tree generator.", "2016/12", "fractalTrees"),
+    new Link("University work", "A page for storing some of my uni work.", "2016/11", "vu"),
+    new Link("Visualgo", "A great page full of algorithm visualizations.", "2016/04", "https://visualgo.net/", true),
+    new Link("Cramer", "A solver for systems of linear equations using the Cramer's rule.", "2015/10", "krameris"),
+    new Link("Matrices", "Implementations of various operations for matrices.", "2015/10", "matricos"),
+    new Link("Permutation mult.", "A little script for multiplying permutations.", "2015/10", "daugintKelinius"),
+    new Link("LineBall", "A little game made with Unity3D.", "2015/10", "lineball"),
+    new Link("Prime factors", "A little script for factoring a number into prime numbers.", "2015/10", "factors"),
+    new Link("Prime checker", "A program for checking, whether a number is prime or not.", "2015/10", "isPrime"),
+    new Link("π", "Get the desired number of digits of π.", "2015/10", "piGen"),
+    new Link("Triangle calc.", "A program for calculating parameters of any triangle.", "2012/10", "triCalc"),
+    new Link("Angle converter", "Converts an angle between radians and degrees.", "2012/10", "rad"),
+    new Link("Quad. eq. calc.", "A quadratic equation solver.", "2012/10", "quad"),
+    new Link("Mult. table", "A multiplication table generator.", "2012/10", "dauglent"),
+    
+];
+
+window.onload = function(){
+    lastEdit();
+    var row = $('#linksRow');
+    for(var i = 0; i < links.length; i++){
+        row.append(buildLinkDiv(links[i]));
+    }
+}
+
+function buildLinkDiv(link){
+    var l = link.isDirect ? link.path : link.path + '/index.html';
+    var img = link.isDirect ? 'partyhard2.gif' : link.path + '/preview.png';
+    var html =  '<div class="col-sm-2">' +
+                    '<div class="link ih-item circle effect13 from_left_and_right">' +
+                        '<a href="'+l+'">' +
+                            '<div class="img"><img src="'+img+'" title="'+link.name+'" alt="'+link.name+'"></div>' +
+                            '<div class="info">' +
+                                '<div class="info-back">' +
+                                    '<h3>'+link.name+'</h3>' +
+                                    '<p>'+link.description+'</p>' +
+                                    '<p>'+link.date+'</p>' +
+                                '</div>' +
+                            '</div>' +
+                        '</a>' +
+                    '</div>' + 
+                '</div>';
+    return html;
+}
+
+
+function lastEdit(){
+    document.getElementById("lastEdited").innerHTML = "Paskutini karta kazkas daryta: "+document.lastModified;
+}
