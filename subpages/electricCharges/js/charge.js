924cfeee588ba181b9272aeb33ce481f12c8e458
vstrimaitis <vstrimaitis@gmail.com>
2017-01-07 20:43:15 +0200

    Modified the electric charges simulation. It now resembles and atom

--- subpages/electricCharges/js/charge.js
+++ subpages/electricCharges/js/charge.js
@@ -1,37 +1,64 @@
 var vstrimaitis = vstrimaitis || {};
 
-vstrimaitis.Charge = function(charge, radius, pos){
+vstrimaitis.maxSpeed = 10;
+
+vstrimaitis.Charge = function(charge, radius, pos, isFixed){
     this.q = charge;
     this.r = radius;
     this.pos = pos;
-    this.k = 1000;
-    this.f = new vstrimaitis.Vector2(0, 0);
+    this.isFixed = isFixed;
+    this.k = 10;
+    this.v = new vstrimaitis.Vector2(0, 0);
     this.applyForce = function(other){
-        if(this.pos == other.pos)
+        if(this.pos == other.pos || this.isFixed)
             return;
         var R = this.pos.subtract(other.pos);
+		if(R.length2 <= Number.EPSILON)
+			return;
         var fMagnitude = this.k * this.q * other.q / (R.length2);
         var F = R.normalize().multiply(fMagnitude);
-        this.f = this.f.add(F);
+        this.v = this.v.add(F);
+        if(this.v.length > vstrimaitis.maxSpeed){
+            this.v = this.v.normalize().multiply(vstrimaitis.maxSpeed);
+        }
     }
 
     this.move = function(){
-        this.pos = this.pos.add(this.f);
-        this.f = new vstrimaitis.Vector2(0, 0);
+        this.pos = this.pos.add(this.v);
+    }
+
+    this.collidesWith = function(other){
+        var r = this.pos.subtract(other.pos);
+        if(r.length <= this.r + other.r){
+            return true;
+        }
+        return false;
     }
 
     this.restrict = function(topLeft, bottomRight){
-        if(this.pos.x < topLeft.x){
-            this.pos = new vstrimaitis.Vector2(topLeft.x, this.pos.y);
-        }
-        if(this.pos.x >= bottomRight.x){
-            this.pos = new vstrimaitis.Vector2(bottomRight.x, this.pos.y);
-        }
-        if(this.pos.y < topLeft.y){
-            this.pos = new vstrimaitis.Vector2(this.pos.x, topLeft.y);
-        }
-        if(this.pos.y >= bottomRight.y){
-            this.pos = new vstrimaitis.Vector2(this.pos.x, bottomRight.y);
-        }
+        /*if(this.pos.x < topLeft.x || this.pos.y < topLeft.y || this.pos.x > bottomRight.x || this.pos.y > bottomRight.y){
+            this.v = this.v.multiply(-1);
+        }*/
+
+
+
+
+
+            if(this.pos.x < topLeft.x){
+                this.pos = new vstrimaitis.Vector2(topLeft.x, this.pos.y);
+            }
+            if(this.pos.x > bottomRight.x){
+                this.pos = new vstrimaitis.Vector2(bottomRight.x, this.pos.y);
+            }
+            if(this.pos.y < topLeft.y){
+                this.pos = new vstrimaitis.Vector2(this.pos.x, topLeft.y);
+            }
+            if(this.pos.y > bottomRight.y){
+                this.pos = new vstrimaitis.Vector2(this.pos.x, bottomRight.y);
+            }
+    }
+
+    this.applyDrag = function(mag){
+        this.v = this.v.multiply(1-mag);
     }
 }
--- subpages/electricCharges/js/index.js
+++ subpages/electricCharges/js/index.js
@@ -2,7 +2,8 @@ var v;
 var vd;
 var backgroundColor = 'rgb(51, 51, 51)';
 var charges = [];
-var maxCharge = 10;
+var maxCharge = 50;
+var drag = 0;//0.01;
 
 window.onload = function(){
     v = vstrimaitis;
@@ -10,50 +11,102 @@ window.onload = function(){
     vd.setCanvas('mainCanvas');
     vd.makeCanvasFullScreen();
 
-    for(var i = 0; i < 80; i++){
-        var r = randomInRange(10, 30);//30;
-        var b = (3-maxCharge)/2;
-        var a = 1-b;
-        var q = r/10 * a + b;
-        //var q = randomInRange(1, maxCharge);
+    /*for(var i = 0; i < 200; i++){
+		var r = 30;
+        var q = randomInRange(-maxCharge, maxCharge);
         charges[i] = new v.Charge(q, r, new v.Point2(randomInRange(r, vd.canvas.width-r), randomInRange(r, vd.canvas.height-r)));
+    }*/
+
+    charges[0] = new v.Charge(100, 50, new v.Point2(vd.canvas.width/2, vd.canvas.height/2), true);
+    charges[1] = new v.Charge(-1, 10, new v.Point2(charges[0].pos.x + 100, charges[0].pos.y));
+    charges[1].v = new v.Point2(0, 2.9);
+
+
+    charges[2] = new v.Charge(-1, 10, new v.Point2(charges[0].pos.x - 200, charges[0].pos.y));
+    charges[2].v = new v.Point2(0, -2.7);
+
+
+    charges[3] = new v.Charge(-1, 10, new v.Point2(charges[0].pos.x, charges[0].pos.y-150));
+    charges[3].v = new v.Point2(2, 0);
+
+
+    var prev = null;
+    window.requestAnimationFrame(function a(timestamp){
+        if(!prev)
+            prev = timestamp;
+        var dt = timestamp - prev;
+        prev = timestamp;
+        update();
+        draw();
+        window.requestAnimationFrame(a);
+    });
+}
+
+function update(){
+    for(var i = 0; i < charges.length; i++){
+        for(var j = 0; j < charges.length; j++){
+            charges[i].applyForce(charges[j]);
+        }
+        charges[i].applyDrag(drag);
     }
-    var interval = setInterval(function(){
-        console.log('frame');
-        for(var i = 0; i < charges.length; i++){
-            for(var j = 0; j < charges.length; j++){
-                charges[i].applyForce(charges[j]);
+    for(var i = 0; i < charges.length; i++){
+        for(var j = i+1; j < charges.length; j++){
+            if(i !== j && charges[i].collidesWith(charges[j])){
+                charges[i].v.multiply(-1);
+                charges[j].v.multiply(-1);
+
+                //charges[i].v = new v.Point2(0,0);
             }
         }
-        //console.log(charges[0].pos);
-        vd.background(backgroundColor);
-        for(var i = 0; i < charges.length; i++){
-            drawCharge(charges[i]);
-            drawForce(charges[i]);
-            charges[i].move();
-            charges[i].restrict(new v.Point2(r, r), new v.Point2(vd.canvas.width-r, vd.canvas.height-r));
-        }
-    }, 10);
+    }
+    for(var i = 0; i < charges.length; i++){
+        charges[i].move();
+        charges[i].restrict(new v.Point2(charges[i].r, charges[i].r), new v.Point2(vd.canvas.width-charges[i].r, vd.canvas.height-charges[i].r));
+    }
+}
 
+function draw(){
+    vd.background(backgroundColor);
+    for(var i = 0; i < charges.length; i++){
+        drawCharge(charges[i]);
+        drawVelocity(charges[i]);
+    }
 }
 
 function chooseColor(c){
-    var b = Math.floor(c.q) * 255/maxCharge;
-    var r = 255 - b;    return 'rgb('+r+', 0, '+b+')';
+    var r, g, b;
+    if(c.q <= 0){
+        r = 255;
+        g = Math.floor((c.q + maxCharge) * 255 / maxCharge);
+        b = g;
+    } else{
+        r = g = Math.floor((c.q-maxCharge) * 255 / (-maxCharge));
+        b = 255;
+    }
+    //var r = Math.floor((c.q-maxCharge)*255/(-2*maxCharge));
+    //var b = 255 - r;
+    return 'rgb(' + r + ', '+g+', ' + b + ')';
+    //var b = Math.floor(c.q) * 255/maxCharge;
+    //var r = 255 - b;    return 'rgb('+r+', 0, '+b+')';
 }
 
 function randomInRange(min, max){
     return Math.random() * (max - min) + min;
 }
 
+function drawVelocity(c){
+    var p1 = c.pos;
+    var p2 = c.pos.add(c.v.multiply(10));
+    vd.arrow(p1, p2);
+    vd.stroke(2, 'black');
+}
+
 function drawCharge(c){
     vd.circle(new v.Point2(c.pos.x, c.pos.y), c.r);
     vd.fill(chooseColor(c));
-}
 
-function drawForce(c){
-    var p1 = c.pos;
-    var p2 = c.pos.add(c.f);
-    vd.line(p1, p2);
-    vd.stroke(1, 'red');
+    /*vd.ctx.font = "30px Arial";
+    vd.ctx.fillStyle = 'white';
+    var vel = "(" + Math.round(c.v.x*100)/100 + ", " + Math.round(c.v.y*100)/100 + ")";
+    vd.ctx.fillText(vel, c.pos.x, c.pos.y);*/
 }
