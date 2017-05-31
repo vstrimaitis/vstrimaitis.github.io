037711f121589fd2ae0abe7818d96d1538d4f4b4
vstrimaitis <vstrimaitis@gmail.com>
2017-05-31 16:24:12 +0300

    Added source.

---
+++ subpages/latexAnimations/js/animationList.js
@@ -0,0 +1,10 @@
+var animations = [
+    {
+        Folder: 'animated_sin',
+        Name: 'Sine wave'
+    },
+    {
+        Folder: 'freefall',
+        Name: 'Freefall'
+    }
+];
--- subpages/latexAnimations/js/index.js
+++ subpages/latexAnimations/js/index.js
@@ -1,28 +1,20 @@
-var animations = [
-    {
-        Folder: 'animated_sin',
-        Name: 'Sine wave'
-    },
-    {
-        Folder: 'freefall',
-        Name: 'Freefall'
-    }
-];
+// Initialise sources and gifs
+for(var i = 0; i < animations.length; i++){
+    var htmls = createButton(animations[i]);
+    $('#buttons_column').append(htmls.button);
+    $('#output_column').append(htmls.output);
+}
 
 $(document).ready(function(){
-    for(var i = 0; i < animations.length; i++){
-        var htmls = createButton(animations[i]);
-        $('#buttons_column').append(htmls.button);
-        $('#output_column').append(htmls.output);
-    }
     $('#output_column').children().hide();
 });
 
 function createButton(animation){
     var buttonHtml = '<button type="button" class="btn btn-default btn-lg btn-block" onclick="showOutput(\''+animation.Folder+'\')">'+animation.Name+'</button>';
     var gifHtml = '<image class="animation_gifs img-responsive" src="animations/'+animation.Folder+'/out.gif"></image>';
-    var sourceHtml = '';
-    var outputHtml = '<div id="'+animation.Folder+'">'+sourceHtml+gifHtml+'</div>';
+    var sourceHtml = '<pre class="language-latex" data-src="animations/'+animation.Folder+'/source.tex"></pre>';
+    var outputHtml = '<div id="'+animation.Folder+'">'+gifHtml+sourceHtml+'</div>';
+    console.log(outputHtml);
     return {button: buttonHtml, output: outputHtml};
 }
 
