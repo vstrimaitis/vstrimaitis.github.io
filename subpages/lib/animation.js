9b8ad6cc68475222a43a0e6fc3a7e2a4c16ef73a
vstrimaitis <vstrimaitis@gmail.com>
2017-07-04 22:45:34 +0300

    Change...

--- subpages/lib/animation.js
+++ subpages/lib/animation.js
@@ -1,12 +1,8 @@
 var vstrimaitis = vstrimaitis || {};
 vstrimaitis._framesPerSecond = 60;
-vstrimaitis._dt = 1/vstrimaitis._framesPerSecond;
-vstrimaitis._accumulator = 0;
-vstrimaitis._previousTimestamp;
 
 vstrimaitis.setFramerate = function(fps){
     vstrimaitis._framesPerSecond = fps;
-    vstrimaitis._dt = 1/fps;
 }
 
 vstrimaitis.animate = function(timestamp){
@@ -15,15 +11,9 @@ vstrimaitis.animate = function(timestamp){
         if(!vstrimaitis._previousTimestamp){
             vstrimaitis._previousTimestamp = timestamp;
         }
-        vstrimaitis._accumulator += timestamp - vstrimaitis._previousTimestamp;
+        vstrimaitis._dt = timestamp - vstrimaitis._previousTimestamp;
         vstrimaitis._previousTimestamp = timestamp;
-        if(vstrimaitis._accumulator > 0.2){
-            vstrimaitis._accumulator = 0.2;
-        }
-        while(vstrimaitis._accumulator > vstrimaitis._dt){
-            update(vstrimaitis._dt);
-            vstrimaitis._accumulator -= vstrimaitis._dt;
-        }
+        update(vstrimaitis._dt);
         draw();
         requestAnimationFrame(vstrimaitis.animate);
     }, 1000/vstrimaitis._framesPerSecond);
